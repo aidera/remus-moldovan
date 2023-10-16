3570a2ea1646645bde960c01a65c73658337b6b7
aidera <hope.aidera@gmail.com>
2023-10-16 19:12:30 -0400

    Home page initial animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,75 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import styles from './page.module.scss';
+import Link from 'next/link';
 
 export default function Home() {
+  const [headersStyles, setHeadersStyles] = useState({});
+  const mainRef = useRef<HTMLDivElement>(null);
+  const headersRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const mainBounds = mainRef.current?.getBoundingClientRect();
+      const mouseX = event.clientX - (mainBounds?.left || 0);
+      const mouseY = event.clientY - (mainBounds?.top || 0);
+      const midpointY = mainBounds!.height / 2;
+
+      const perspective = 1000;
+      const rotationX =
+        mouseY < midpointY
+          ? (mouseY / midpointY - 1) * -15 // Positive rotation for the top half
+          : (1 - mouseY / midpointY) * 15; // Negative rotation for the bottom half
+      const rotationY = (mouseX / mainBounds!.width - 0.5) * 15;
+
+      const dynamicStyles = {
+        transform: `perspective(${perspective}px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`,
+      };
+
+      setHeadersStyles(dynamicStyles);
+    };
+
+    if (mainRef.current) {
+      mainRef.current.addEventListener('mousemove', handleMouseMove);
+    }
+
+    return () => {
+      if (mainRef.current) {
+        mainRef.current.removeEventListener('mousemove', handleMouseMove);
+      }
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main ref={mainRef} className={styles.main}>
+      <video
+        className={styles.bgVideo}
+        autoPlay
+        loop
+        muted
+        poster="https://assets.codepen.io/6093409/river.jpg"
+      >
+        <source src="/home-background-video.mp4" type="video/mp4" />
+      </video>
+
+      <div className={styles.content}>
+        <div ref={headersRef} className={styles.headers} style={headersStyles}>
+          <h1>Remus Moldovan</h1>
+
+          <div className={styles.quoteContainer}>
+            <q>...to get on top of the hill you need to dig deep</q>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <Link href="/pages/about">ABOUT</Link>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <nav>
+          <Link className={styles.accentLink} href="/art">
+            Explore the art
+          </Link>
+          <Link href="/about">About the artist</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
       </div>
     </main>
-  )
+  );
 }
