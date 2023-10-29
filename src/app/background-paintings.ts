6b6348f02eec06d315b3b732e5efba58212bb15d
aidera <hope.aidera@gmail.com>
2023-10-29 14:08:22 -0400

    Video strategy changed

---
+++ src/app/background-paintings.ts
@@ -0,0 +1,38 @@
+export const BG_PAINTINGS = [
+  {
+    id: 1,
+    title: "It's How I See-1993",
+    time: 0,
+    link: '/link1',
+  },
+  {
+    id: 2,
+    title: 'Painting 2',
+    time: 2.9,
+    link: '/link2',
+  },
+  {
+    id: 3,
+    title: 'Remember-1993',
+    time: 7.2,
+    link: '/link3',
+  },
+  {
+    id: 4,
+    title: 'The Spiral-2002',
+    time: 11.25,
+    link: '/link4',
+  },
+  {
+    id: 5,
+    title: 'The Beginning-1991',
+    time: 15.3,
+    link: '/link5',
+  },
+  {
+    id: 6,
+    title: 'Nimpha-1993',
+    time: 19.1,
+    link: '/link6',
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,64 +3,37 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.scss';
 import Link from 'next/link';
+import { BG_PAINTINGS } from './background-paintings';
 
 export default function Home() {
   const mainRef = useRef<HTMLDivElement>(null);
   const headersRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
-  const [headersStyles, setHeadersStyles] = useState({});
-  const [videoAnimationKey1, setVideoAnimationKey1] =
-    useState<boolean>(false);
-  const [videoAnimationKey2, setVideoAnimationKey2] =
-    useState<boolean>(false);
+  const [videoAnimationKey, setVideoAnimationKey] = useState<number>(0);
+  const currentPainting =
+    BG_PAINTINGS.find((el) => el.id === videoAnimationKey) || null;
 
   const handleTimeUpdate = () => {
-    if (
-      videoRef.current &&
-      videoRef.current.currentTime >= 1 &&
-      !videoAnimationKey1
-    ) {
-      setVideoAnimationKey1(true);
+    if (!videoRef.current) {
+      return;
     }
 
-    if (
-      videoRef.current &&
-      videoRef.current.currentTime >= 6 &&
-      !videoAnimationKey2
-    ) {
-      setVideoAnimationKey2(true);
+    for (const painting of BG_PAINTINGS) {
+      if (videoRef.current.currentTime >= painting.time) {
+        setVideoAnimationKey(painting.id);
+      }
     }
   };
 
   useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      const mainBounds = mainRef.current?.getBoundingClientRect();
-      const mouseX = event.clientX - (mainBounds?.left || 0);
-      const mouseY = event.clientY - (mainBounds?.top || 0);
-      const midpointY = mainBounds!.height / 2;
-
-      const perspective = 1000;
-      const rotationX =
-        mouseY < midpointY
-          ? (mouseY / midpointY - 1) * -15 // Positive rotation for the top half
-          : (1 - mouseY / midpointY) * 15; // Negative rotation for the bottom half
-      const rotationY = (mouseX / mainBounds!.width - 0.5) * 15;
-
-      const dynamicStyles = {
-        transform: `perspective(${perspective}px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`,
-      };
-
-      setHeadersStyles(dynamicStyles);
-    };
-
-    if (mainRef.current) {
-      mainRef.current.addEventListener('mousemove', handleMouseMove);
+    if (videoRef.current) {
+      videoRef.current.addEventListener('timeupdate', handleTimeUpdate);
     }
 
     return () => {
-      if (mainRef.current) {
-        mainRef.current.removeEventListener('mousemove', handleMouseMove);
+      if (videoRef.current) {
+        videoRef.current.removeEventListener('timeupdate', handleTimeUpdate);
       }
     };
   }, []);
@@ -72,32 +45,15 @@ export default function Home() {
         onTimeUpdate={handleTimeUpdate}
         className={styles.bgVideo}
         autoPlay
+        loop
         muted
         poster="/Video-sequence-first.jpg"
       >
-        <source src="/Video-background.mp4" type="video/mp4" />
+        <source src="/Sequence 01_1.mp4" type="video/mp4" />
       </video>
 
-      <div
-        className={
-          styles.initialContent +
-          (!videoAnimationKey1 ? ' ' + styles.initialContent__active : '')
-        }
-      >
-        <div className={styles.initialContent_background}></div>
-        <div className={styles.initialContent_contentWrapper}>
-          <img src="/logo-2.png" />
-          <span>It's How I See</span>
-        </div>
-      </div>
-
-      <div
-        className={
-          styles.mainContent +
-          (videoAnimationKey2 ? ' ' + styles.mainContent__active : '')
-        }
-      >
-        <div ref={headersRef} className={styles.headers} style={headersStyles}>
+      <div className={styles.mainContent}>
+        <div ref={headersRef} className={styles.headers}>
           <h1>Remus Moldovan</h1>
 
           <div className={styles.quoteContainer}>
@@ -113,6 +69,15 @@ export default function Home() {
           <Link href="/contact">Contact</Link>
         </nav>
       </div>
+
+      {currentPainting && (
+        <div className={styles.paintingLinkContainer}>
+          <span>On the background: </span>
+          <Link className={styles.paintingLink} href={currentPainting.link}>
+            {currentPainting.title}
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
